482cc0574ec8d9173fb466df622f451a277b0aaf
Ammar Al-sayari <aalsayari0027@stu.kau.edu.sa>
2023-08-23 10:42:41 +0300

    set-up

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    
+
+      <div className="p-6">
+        <Button size="default" variant="default">Click me</Button>
+      </div>
+      
+      
+    
+    
+    
+  )
+}
