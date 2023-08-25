a73ba69a006f45461831a0e696502ab31d5270a0
Ammar Al-sayari <aalsayari0027@stu.kau.edu.sa>
2023-08-25 17:31:18 +0300

    day2clerk2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,12 @@
-import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetUpPage = () => {
   return (
-    
-
       <div className="p-6">
-        <Button size="default" variant="default">Click me</Button>
+       <UserButton afterSignOutUrl="/"/>
       </div>
       
-      
-    
-    
-    
   )
 }
+
+export default SetUpPage;
