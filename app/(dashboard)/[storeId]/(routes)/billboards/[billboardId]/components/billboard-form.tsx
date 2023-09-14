b5c0fb8db4b180584a990fb72357655cc934e941
Ammar Al-sayari <aalsayari0027@stu.kau.edu.sa>
2023-09-14 07:54:24 +0300

    d10.2 billboards APIs

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -23,6 +23,7 @@ import { Separator } from "@/components/ui/separator";
 import { AlertModal } from "@/components/modals/alert-modal";
 import { ApiAlert } from "@/components/ui/api-alert";
 import { useOrigin } from "@/hooks/use-origin";
+import ImageUpload from "@/components/ui/image-upload";
 
 const formSchema = z.object({
     label: z.string().min(1),
@@ -64,9 +65,14 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
     const onSubmit = async (data: BillboardFormValues) => {
         try {
             setLoading(true);
-            await axios.patch(`/api/stores/${params.storeId}`, data)
+            if(initialData){
+                await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data)
+            }else {
+                await axios.post(`/api/${params.storeId}/billboards`, data)
+            }
             router.refresh();
-            toast.success("Store updated.");
+            router.push(`/${params.storeId}/billboards`);
+            toast.success(toastMessage);
         } catch (error) {
             toast.error("Something went wrong.");
         }finally {
@@ -78,12 +84,12 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
         try {
             
             setLoading(true);
-            await axios.delete(`/api/stores/${params.storeId}`);
+            await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
             router.refresh();
             router.push("/");
-            toast.success("Store deleted.");
+            toast.success("Billboard deleted.");
         } catch (error) {
-            toast.error("Make sure you have no products in your store.");
+            toast.error("Make sure you have no catagories or products in this billboard.");
         } finally {
             setLoading(false);
             setOpen(false);
@@ -119,6 +125,24 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
             <Separator/>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full" >
+                    <FormField
+                        control={form.control}
+                        name="imageUrl"
+                        render={({field}) =>(
+                            <FormItem>
+                                <FormLabel>Background image</FormLabel>
+                                <FormControl>
+                                    <ImageUpload 
+                                        value={field.value ? [field.value] : []} 
+                                        disabled={loading}
+                                        onChange={(url) => field.onChange(url)}
+                                        onRemove={() => field.onChange("")}
+                                    />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
                     <div className="grid grid-cols-3 gap-8">
                         <FormField
                         control={form.control}
